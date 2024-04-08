38dde3583d6b6e3ad34a05fb3481170d204c1d63
Ny Avoko <Ny Avoko@Avoko.labooi.mg>
2024-04-08 22:25:26 +0300

    Tp2 vers début Tp3

---
+++ TP3deNyAvoko/src/main/java/com/nyavoko/tp2denyavoko/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nyavoko.tp2denyavoko.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ *
+ * @author Avoko NIVOARISON
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
